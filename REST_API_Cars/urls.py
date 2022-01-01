ab34aca2f8d400f3a34a4775309ac02beecec261
Maciej <maciej.szulgacz@gmail.com>
2022-01-01 19:52:52 +0100

    edit url

--- REST_API_Cars/urls.py
+++ REST_API_Cars/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from cars.views import CreateCarView, DeleteCarView, CreateRateView, PopularCarsView, ListOfCarsView
+from cars.views import CreateCarView, DeleteCarView, CreateRateView, PopularCarsView, WelcomeView
 from rest_framework.routers import SimpleRouter
 
 
@@ -27,7 +27,7 @@ urlpatterns = [
     path('cars/', CreateCarView.as_view(), name='create_car'),
     # path('cars/', FindCarView.as_view(), name='find_car'),
     path('delete/<int:car_id>', DeleteCarView.as_view(), name='delete'),
-    path('', ListOfCarsView.as_view(), name='base'),
+    path('', WelcomeView.as_view(), name='base'),
     path('rate', CreateRateView.as_view(), name='create_rate'),
     path('rate/', CreateRateView.as_view(), name='create_rate'),
     # path('list_of_rates', ListOfRatesView.as_view(), name='list_of_rates'),
