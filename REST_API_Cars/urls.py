4cacc569067e4b7ec4966ce50926b251492e21bf
Maciej <maciej.szulgacz@gmail.com>
2022-01-02 19:15:54 +0100

    check if car exists in API

--- REST_API_Cars/urls.py
+++ REST_API_Cars/urls.py
@@ -1,18 +1,3 @@
-"""REST_API_Cars URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from cars.views import CreateCarView, DeleteCarView, CreateRateView, PopularCarsView, WelcomeView
@@ -25,13 +10,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cars', CreateCarView.as_view(), name='create_car'),
     path('cars/', CreateCarView.as_view(), name='create_car'),
-    # path('cars/', FindCarView.as_view(), name='find_car'),
     path('delete/<int:car_id>', DeleteCarView.as_view(), name='delete'),
     path('', WelcomeView.as_view(), name='base'),
     path('rate', CreateRateView.as_view(), name='create_rate'),
     path('rate/', CreateRateView.as_view(), name='create_rate'),
-    # path('list_of_rates', ListOfRatesView.as_view(), name='list_of_rates'),
-    # path('rate/', FindRateView.as_view(), name='find_rate'),
     path('popular', PopularCarsView.as_view(), name='popular_cars'),
     path('popular/', PopularCarsView.as_view(), name='popular_cars'),
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,10 +1,10 @@
-from sqlite3 import ProgrammingError
-
-from django.http import JsonResponse
+from django.contrib.sites import requests
+from django.http import JsonResponse, HttpResponse
 from rest_framework import views
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from django.db.models import Avg
+import requests
 
 from .serializers import CarSerializer, RateSerializer
 from .models import Car, Rate
@@ -14,7 +14,7 @@ class CreateCarView(views.APIView):
     def get(self, request):
         queryset_of_cars = Car.objects.all().values()
         if not queryset_of_cars:
-            return JsonResponse("Database is empty, please send data to the database.", safe=False)
+            return HttpResponse("Database is empty, please send some data to the database.")
         else:
             list_of_cars = []
             for car in queryset_of_cars:
@@ -29,36 +29,31 @@ class CreateCarView(views.APIView):
             return JsonResponse(list_of_cars, safe=False)
 
     def post(self, request):
-        serializer = CarSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-
-# class FindCarView(views.APIView):
-#     def post(self, request):
-#         dict_car_id = request.data
-#         car_id = dict_car_id['id']
-#         find_car = Car.objects.filter(id=car_id).values()
-#         car_make = find_car[0]['make']
-#         car_model = find_car[0]['model']
-#         dict_car = {
-#             "make": car_make,
-#             "model": car_model
-#         }
-#         return JsonResponse(dict_car)
+        dict_car_id = request.data
+        car_make = dict_car_id['make']
+        car_model = dict_car_id['model']
+        response_from_api = requests.get(f"https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/{car_make}?format=json")
+        data_from_api = response_from_api.json()
+        result_from_api = data_from_api['Results']
+        if not any(a['Model_Name'] == f"{car_model}" for a in result_from_api):
+            return HttpResponse("Please, provide correct data.")
+        else:
+            serializer = CarSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
 
 
 class WelcomeView(views.APIView):
     def get(self, request):
-        return JsonResponse("Welcome on my APP", safe=False)
+        return HttpResponse("Welcome on my application Cars_REST_API.")
 
 
 class DeleteCarView(views.APIView):
     def delete(self, request, car_id):
         obj = get_object_or_404(Car, id=car_id)
         obj.delete()
-        return JsonResponse("Car was deleted successfully.", safe=False)
+        return HttpResponse("Car was deleted successfully.")
 
 
 class CreateRateView(views.APIView):
@@ -69,42 +64,22 @@ class CreateRateView(views.APIView):
         return Response(serializer.data)
 
 
-# class ListOfRatesView(views.APIView):
-#     def get(self, request):
-#         list_of_rates = Rate.objects.all()
-#         rates_serializer = RateSerializer(list_of_rates, many=True)
-#         return JsonResponse(rates_serializer.data, safe=False)
-
-
-# class FindRateView(views.APIView):
-#     def post(self, request):
-#         dict_of_data = request.data
-#         rate_id = dict_of_data['id']
-#         find_rate = Rate.objects.filter(id=rate_id).values()
-#         my_rate = Rate.objects.get(id=rate_id)
-#         my_car = my_rate.cars.all().values()
-#         car_id = my_car[0]['id']
-#         rate = find_rate[0]['rate']
-#         dict_rate = {
-#             "car_id": car_id,
-#             "rating": rate,
-#         }
-#         return JsonResponse(dict_rate)
-
-
 class PopularCarsView(views.APIView):
     def get(self, request):
-        queryset_of_cars = Car.objects.all().values()
-        list_of_cars = []
-        for car in queryset_of_cars:
-            rate = Rate.objects.filter(cars=car['id']).count()
-            dict_car = {
-                "id": car['id'],
-                "make": car['make'],
-                "model": car['model'],
-                "rates_number": rate,
-            }
-            list_of_cars.append(dict_car)
-            sorted_list_of_cars = sorted(list_of_cars, key=lambda x: -x['rates_number'])
-            top_list_of_cars = sorted_list_of_cars[0:2]
-        return JsonResponse(top_list_of_cars, safe=False)
+        try:
+            queryset_of_cars = Car.objects.all().values()
+            list_of_cars = []
+            for car in queryset_of_cars:
+                rate = Rate.objects.filter(cars=car['id']).count()
+                dict_car = {
+                    "id": car['id'],
+                    "make": car['make'],
+                    "model": car['model'],
+                    "rates_number": rate,
+                }
+                list_of_cars.append(dict_car)
+                sorted_list_of_cars = sorted(list_of_cars, key=lambda x: -x['rates_number'])
+                top_list_of_cars = sorted_list_of_cars[0:2]
+            return JsonResponse(top_list_of_cars, safe=False)
+        except UnboundLocalError:
+            return HttpResponse("Database is empty, please send some data to the database.")
