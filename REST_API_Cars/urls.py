84ae947ac5611fc37212398b865ee91e07ac4b95
Maciej <maciej.szulgacz@gmail.com>
2022-02-22 21:04:39 +0100

    update urls

--- REST_API_Cars/urls.py
+++ REST_API_Cars/urls.py
@@ -5,12 +5,9 @@ from cars.views import CreateCarView, CreateRateView, DeleteCarView, PopularCars
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("cars", CreateCarView.as_view(), name="create_car"),
     path("cars/", CreateCarView.as_view(), name="create_car"),
-    path("delete/<int:car_id>", DeleteCarView.as_view(), name="delete"),
+    path("delete/<int:car_id>/", DeleteCarView.as_view(), name="delete"),
     path("", WelcomeView.as_view(), name="base"),
-    path("rate", CreateRateView.as_view(), name="create_rate"),
     path("rate/", CreateRateView.as_view(), name="create_rate"),
-    path("popular", PopularCarsView.as_view(), name="popular_cars"),
     path("popular/", PopularCarsView.as_view(), name="popular_cars"),
 ]
